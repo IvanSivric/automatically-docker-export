fec6424ef81c091f815cf7574f26796cb0968d10
Ivan S <11981084+IvanSivric@users.noreply.github.com>
2018-09-19 17:40:43 +0200

    add path args to store tar

--- automatically-docker-export.sh
+++ automatically-docker-export.sh
@@ -5,8 +5,7 @@ set -u
 
 CONTAINER_NAMES=$(docker ps --format "{{.Names}}")
 for container_name in $CONTAINER_NAMES; do
-    ARCHIVE_NAME="$container_name-$(date +"%m%d%y_%H%M%S").tar"
+    ARCHIVE_NAME=$container_name-$(date +"%m%d%y_%H%M%S").tar
     echo "Exporting $container_name to archive $ARCHIVE_NAME ."
-    docker export $container_name > $ARCHIVE_NAME
+    docker export $container_name > $1/$ARCHIVE_NAME
 done
-
